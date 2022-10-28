cad201ac867bf7e842fde6b62c71e5fc9c4b0a31
Eduardo Andre Soares <eduardo.andre.soares@hp.com>
2022-10-28 10:26:32 -0300

    Remove commented out code

--- examples/auth/remix-auth/app/supabase.ts
+++ examples/auth/remix-auth/app/supabase.ts
@@ -18,25 +18,9 @@ if (!process.env.SUPABASE_SERVICE_KEY) {
   throw new Error('SUPABASE_SERVICE_KEY is required')
 }
 
-// Supabase options example (build your own :))
-// https://supabase.com/docs/reference/javascript/initializing#with-additional-parameters
-
-// const supabaseOptions = {
-//   fetch, // see ⚠️ cloudflare
-//   schema: "public",
-//   persistSession: true,
-//   autoRefreshToken: true,
-//   detectSessionInUrl: true,
-//   headers: { "x-application-name": "{my-site-name}" }
-// };
-
-// ⚠️ cloudflare needs you define fetch option : https://github.com/supabase/supabase-js#custom-fetch-implementation
-// Use Remix fetch polyfill for node (See https://remix.run/docs/en/v1/other-api/node)
-
 export const supabaseClient = createClient(
   process.env.SUPABASE_URL,
   process.env.SUPABASE_SERVICE_KEY
-  // supabaseOptions
 )
 
 export { Session }
