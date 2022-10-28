73b340484689614cb4f53a9aeca18a2d096f231d
Eduardo Andre Soares <eduardo.andre.soares@hp.com>
2022-10-28 09:55:40 -0300

    Remove commented out code

--- apps/temp-docs/data/github.js
+++ apps/temp-docs/data/github.js
@@ -1,6 +1,5 @@
 import supabase from './repos/supabase.json'
 import realtime from './repos/realtime.json'
-// import marketplace from './repos/marketplace.json'
 import postgres from './repos/postgres.json'
 import doctestJs from './repos/doctest-js.json'
 import postgrestJs from './repos/postgrest-js.json'
